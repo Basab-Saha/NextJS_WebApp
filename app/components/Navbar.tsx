231b5d42cff843f7050b08c0cf1723e0276a85a8
Basab Saha <ronylovescoding999@gmail.com>
2023-12-31 17:01:02 +0530

    Account image showed by configuring nextjs config file by providing hostname & PopUp box visibility on clicking on accounts logo- Commit9

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,9 +3,24 @@
 import { useSession , signOut } from "next-auth/react";
 import Link from "next/link";
 
+import Image from "next/image";
+import { useState } from "react";
+
 export default function Navbar(){
 
-    const {status }=useSession();
+    const {status  , data:session }=useSession();
+
+    const[isVisible , setIsVisible]=useState(false);
+
+    const handleAccountClick=()=>{
+        if(isVisible==true){
+            setIsVisible(false);
+        }
+        else{
+            setIsVisible(true);
+        }
+    }
+    
 
     return(
         
@@ -20,10 +35,40 @@ export default function Navbar(){
             {
 
                 status==="authenticated" ? 
-                (<div>
-                    <img className="h-10 w-10  rounded-xl" src="https://lh3.googleusercontent.com/a/ACg8ocIRKhbC2YM5cb4xDrjvglBwFc2X6qHBVnxPvISGqQAlnRI=s96-c"/>
+
+                (<div className="flex flex-col gap-4">
+                    
+                    <div className="flex items-center gap-3">
+                   
+                     <Link href='/create-post'  className="btn flex gap-2"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+</svg>
+Create Post</Link>
+
+                      {/*Profile image*/}
+                      <div className="relative rounded-full overflow-hidden w-8 h-8">
+                        <Image
+                        src={session?.user?.image || ""}
+                        layout="fill"
+                        objectFit="cover"
+                        alt=""
+                        onClick={handleAccountClick}
+                        className="cursor-pointer hover:scale-105"
+                        />
+                      </div>
+                </div>
+
+                <div className={`shadow-sm px-3 py-2 flex flex-col gap-2 h-30 w-auto ${isVisible==true ? "":"hidden"}`}>
+                    <h1  className="hover:underline font-bold text-2xl">{session?.user?.name}</h1>
+                    <h1 className="hover:underline">{session?.user?.email}</h1>
+                  <Link href='/dashboard'> <h2 className="hover:underline">My Dashboard</h2></Link> 
                     <button onClick={()=>signOut()}  className="btn">Sign Out</button>
-                </div>) :
+
+                </div>
+                    
+                </div>
+                
+                ) :
                 (
                     <div className="flex items-center">
                     <Link className="btn" href={"/sign-in"}>Sign In</Link>
